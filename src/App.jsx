b689151d79fba3df878185fa6613edcf841e7be0
suvadip717 <suvadipdas717@gmail.com>
2024-01-26 11:40:23 +0530

    complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,42 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Card from "./components/CardUi";
 import ThemeBtn from "./components/ThemeBtn";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () =>{
+    setThemeMode("light")
+  }
+  const darkTheme = () =>{
+    setThemeMode("dark")
+  }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove('light','dark')
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
   return (
-    <>
-      <div className="flex flex-wrap min-h-screen items-center">
+    <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
+      <div className="grid place-content-center h-screen mx-auto p-10">
         <div className="w-1/4">
-          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
-          <ThemeBtn/>
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          <ThemeBtn />
           <div className="w-full max-w-sm mx-auto"></div>
-          <Card/>
+          <Card />
         </div>
       </div>
-    </>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+
+// <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+//           <ThemeBtn />
+//           <div className="w-full max-w-sm mx-auto"></div>
+//           <Card />
