8287d64c336ae3c1978dcf4caa33eb088812a768
suvadip717 <suvadipdas717@gmail.com>
2024-01-26 08:34:25 +0530

    complete UI design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+import "./App.css";
+import Card from "./components/CardUi";
+import ThemeBtn from "./components/ThemeBtn";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-1/4">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          <ThemeBtn/>
+          <div className="w-full max-w-sm mx-auto"></div>
+          <Card/>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
